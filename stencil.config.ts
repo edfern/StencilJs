4b971573556d080a751d329bb90757c06e54ea93
Fernando P_ <ed_fern@hotmail.com>
2020-11-18 13:24:56 -0600

    second commint

--- stencil.config.ts
+++ stencil.config.ts
@@ -3,7 +3,6 @@ import { Config } from '@stencil/core';
 // https://stenciljs.com/docs/config
 
 export const config: Config = {
-  namespace:'app-hospital',
   globalStyle: 'src/global/app.css',
   globalScript: 'src/global/app.ts',
   taskQueue: 'async',
